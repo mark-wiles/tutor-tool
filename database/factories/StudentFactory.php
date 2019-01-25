7e2b1e2a3804cfe2f7777410f0f1c43039a2c8d2
phpdev115 <goldendiscount01@gmail.com>
2019-01-24 21:31:47 -0800

    updated student factory

--- database/factories/StudentFactory.php
+++ database/factories/StudentFactory.php
@@ -4,6 +4,14 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Student::class, function (Faker $faker) {
     return [
-        //
+        'first_name' => $faker->firstName,
+        'last_name' => $faker->lastName,
+        'street' => $faker->streetAddress,
+        'city' => $faker->city,
+        'state' => $faker->state,
+        'zip' => $faker->postcode,
+        'phone' => $faker->phoneNumber,
+        'email' => $faker->unique()->safeEmail,
+        'user_id' => 3,
     ];
 });
