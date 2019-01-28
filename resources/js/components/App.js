f168bb3430fa46b7f708762bf86ec08e44913efd
phpdev115 <goldendiscount01@gmail.com>
2019-01-28 13:05:31 -0800

    added react routes

--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Lessons from './Lessons';
+import Messages from './Messages';
 import NavbarBottom from './NavbarBottom';
-import NavbarTop from './NavbarTop';
 import Student from './Student';
+import StudentNew from './StudentNew';
 import Students from './Students';
+import Earnings from './Earnings';
 
 
 export default class App extends Component {
@@ -14,12 +16,14 @@ export default class App extends Component {
         return (
             <Router>
                 <div className="app-container col-sm-12">
-                    <NavbarTop />
 
                     <Switch>
                         <Route exact path="/home" component={Students} />
+                        <Route exact path="/student/new" component={StudentNew} />
                         <Route exact path="/student/:id" component={Student} />
                         <Route exact path="/lessons" component={Lessons} />
+                        <Route exact path="/messages" component={Messages} />
+                        <Route exact path="/earnings" component={Earnings} />
                     </Switch>
 
                     <NavbarBottom />
