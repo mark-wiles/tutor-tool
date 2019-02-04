85049277e109b3d0ffea3e01bf3e2ea6c962eb01
phpdev115 <goldendiscount01@gmail.com>
2019-02-04 14:43:06 -0800

    added payment to fillable

--- app/Lesson.php
+++ app/Lesson.php
@@ -6,10 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Lesson extends Model
 {
-    protected $fillable = ['start_time', 'end_time', 'rate', 'student_id', 'user_id', 'location', 'subject'];
+    protected $fillable = ['start_time', 'end_time', 'rate', 'student_id', 'user_id', 'location', 'subject', 'payment'];
 
 	public function user()
+
 	  {
+
 		return $this->belongsTo(User::class);
-	  }
+
+		}
+		
 }
