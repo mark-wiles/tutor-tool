51e9f16f0f69792ae24b8244bfa7beeb766d2725
phpdev115 <goldendiscount01@gmail.com>
2019-02-09 23:01:36 -0800

    updated lessons

--- resources/js/components/Lessons/Lessons.js
+++ resources/js/components/Lessons/Lessons.js
@@ -1,45 +1,103 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import NavbarTop from '../NavbarTop';
+import moment from 'moment';
 
 class Lessons extends Component {
 
 	constructor(props) {
 		super(props);
 		this.state = {
+			moment: moment().format("MM-DD-YYYY"),
 			lessons: []
 		};
+		this.handleClick = this.handleClick.bind(this);
 	}
 
-	// componentDidMount() {
-	// 	axios.get('/lessons'
-	// 	)
-	// 	.then((response) => {
-	// 		var lessons = response.data;
-	// 		this.setState({lessons});
-	// 	})
-	// 	.catch((error) => {
-	// 		console.log(error)
-	// 	});
-	// }
+	componentDidMount() {
+		axios.get('/lessons'
+		)
+		.then((response) => {
+			let lessons = response.data;
+			this.setState({lessons});
+		})
+		.catch((error) => {
+			console.log(error);
+		});
+	}
+
+	handleDate(date) {
+		var theDate = new Date(date);
+		theDate = moment(theDate).format('ddd, MMM D YYYY');
+		return theDate;
+	}
+
+	handleTime(time) {
+		var theTime = new Date(time);
+		theTime = moment(theTime.getTime()).format('h:mm A');
+		return theTime;
+	}
+
+	handleClick() {
+		event.preventDefault();
+		var url;
+		switch (event.target.id) {
+			case 'unsubmitted':
+				url = '/lessons/unsubmitted';
+				break;
+			case 'submitted':
+				url = '/lessons/submitted';
+				break;
+			default:
+				url = '/lessons';
+		}
+		axios.get(url
+		)
+		.then((response) => {
+			var lessons = response.data;
+			this.setState({lessons});
+		})
+		.catch((error) => {
+			console.log(error);
+		});
+	}
 
 	render() {
-
-		// const allLessons = this.state.lessons.map((lesson) =>
+		
+		const allLessons = this.state.lessons.map((lesson) =>
+				<div className="lesson-summary" key={lesson.id}>
+					<Link to={'lesson/' + lesson.id}>
+						<h5 className="font-weight-bold">{ this.handleDate(lesson.start_time) }</h5>
+						<h5 className="mb-1">{lesson.first_name} {lesson.last_name}</h5>
+						<h6 className="mb-0">{ this.handleTime(lesson.start_time) } - { this.handleTime(lesson.end_time) }</h6>
+					</Link>
+				</div>
 			
-		// 		<div className="lesson-summary" key={lesson.id}>
-		// 			<Link to={'lesson/' + lesson.id}>
-		// 				<h4 className="font-weight-bold mb-1">{lesson.first_name}</h4>
-		// 				<h6>{lesson.city}, {lesson.state} {lesson.zip}</h6>
-		// 			</Link>
-		// 		</div>
-			
-		// )
+		)
 
 		return(
-			<div className="content-container">
-				<h2 className="font-weight-bold">Lessons</h2>
-				{/* { allLessons } */}
+			<div className="row">
+				<NavbarTop 
+					classLeft="fas fa-arrow-left orange"
+					classRight="fas fa-plus orange"
+					linkLeft="/home"
+					linkRight="/lesson/new"
+					title="Lessons"
+				/>
+				
+				<div className="container content-container">
+
+					<div className="lesson-buttons row">
+							<div className="lesson-btn orange" id="upcoming" onClick={this.handleClick}>Upcoming</div>
+
+							<div className="lesson-btn orange" id="unsubmitted" onClick={this.handleClick}>Unsubmitted</div>
+
+							<div className="lesson-btn orange" id="submitted" onClick={this.handleClick}>Submitted</div>
+					</div>
+
+					{ allLessons }
+				</div>
 			</div>
 		);
 	}
