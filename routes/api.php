4a311dc17d21848abc070d84c82444c50832e457
phpdev115 <goldendiscount01@gmail.com>
2019-02-11 11:32:02 -0800

    moved routes from web to api

--- routes/api.php
+++ routes/api.php
@@ -2,17 +2,14 @@
 
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::get('/students', 'StudentController@index');
+Route::get('/student/{id}', 'StudentController@show');
+Route::post('/student', 'StudentController@store');
+Route::put('/student/{student}', 'StudentController@update');
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/lessons', 'LessonController@index');
+Route::get('/lesson/{id}', 'LessonController@show');
+Route::get('/lessons/unsubmitted', 'LessonController@unsubmitted');
+Route::get('/lessons/submitted', 'LessonController@submitted');
+Route::post('/lesson', 'LessonController@store');
+Route::put('/lesson/{lesson}', 'LessonController@update');
