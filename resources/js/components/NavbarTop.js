2092fd8f9afbd8a500e9be7c0bfdd2923456a694
phpdev115 <goldendiscount01@gmail.com>
2019-02-11 11:27:56 -0800

    changed to functional component

--- resources/js/components/NavbarTop.js
+++ resources/js/components/NavbarTop.js
@@ -1,39 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import axios from 'axios';
 import Icon from './Icon';
 
-class NavbarTop extends Component {
+const NavbarTop = (props) => (
 
-	constructor(props) {
-		super(props);
-	}
-
-	// componentDidMount() {
-	// 	console.log(this.props);
-	// }
-
-	// handleLogout() {
-	// 	event.preventDefault();
-	// 	axios.post('/logout', {
-	// 	  })
-	// 	  .then(function (response) {
-	// 		window.location.replace('/');
-	// 	  })
-	// 	  .catch(function (error) {
-	// 		console.log(error);
-	// 	  });
-	// }
-
-	render() {
-		return(
 			<nav className="container nav-top">
-				<Link to={this.props.linkLeft}><Icon className={this.props.classLeft} /></Link>
-				<h2 className="mb-0 text-center">{this.props.title}</h2>
-				<Link to={this.props.linkRight}><Icon className={this.props.classRight} /></Link>
+				<Link to={props.linkLeft}><Icon className={props.classLeft} /></Link>
+				<h2 className="mb-0 text-center">{props.title}</h2>
+				<Link to={props.linkRight}><Icon className={props.classRight} /></Link>
 			</nav>
 		)
-	}
-}
 
 export default NavbarTop;
