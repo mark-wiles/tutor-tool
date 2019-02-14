7957a6bd45812e187bc36c20775419de6305e059
phpdev115 <goldendiscount01@gmail.com>
2019-02-13 18:31:35 -0800

    added post route for notes

--- routes/api.php
+++ routes/api.php
@@ -2,14 +2,16 @@
 
 use Illuminate\Http\Request;
 
-Route::get('/students', 'StudentController@index');
-Route::get('/student/{id}', 'StudentController@show');
-Route::post('/student', 'StudentController@store');
-Route::put('/student/{student}', 'StudentController@update');
-
 Route::get('/lessons', 'LessonController@index');
 Route::get('/lesson/{id}', 'LessonController@show');
 Route::get('/lessons/unsubmitted', 'LessonController@unsubmitted');
 Route::get('/lessons/submitted', 'LessonController@submitted');
 Route::post('/lesson', 'LessonController@store');
 Route::put('/lesson/{lesson}', 'LessonController@update');
+
+Route::post('/note', 'NoteController@store');
+
+Route::get('/students', 'StudentController@index');
+Route::get('/student/{id}', 'StudentController@show');
+Route::post('/student', 'StudentController@store');
+Route::put('/student/{student}', 'StudentController@update');
