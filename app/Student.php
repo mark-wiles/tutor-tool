71f3b2af0f4caaaaf8704b8a96af80e43ffff586
phpdev115 <goldendiscount01@gmail.com>
2019-02-13 18:34:49 -0800

    added notes relationship

--- app/Student.php
+++ app/Student.php
@@ -11,5 +11,10 @@ class Student extends Model
 	public function user()
 	  {
 		return $this->belongsTo(User::class);
+		}
+		
+		public function notes()
+	  {
+		return $this->hasMany(Note::class);
 	  }
 }
