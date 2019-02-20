1b76701a5351df44828ecb47d21393ef1755ab3e
phpdev115 <goldendiscount01@gmail.com>
2019-02-20 10:42:47 -0800

    added get, put, and delete routes

--- routes/api.php
+++ routes/api.php
@@ -2,21 +2,28 @@
 
 use Illuminate\Http\Request;
 
+//address routes
 Route::get('/address/{address}', 'AddressController@show');
 Route::post('/address', 'AddressController@store');
 Route::put('/address/{address}', 'AddressController@update');
 Route::delete('/address/{address}', 'AddressController@destroy');
 
+//lesson routes
 Route::get('/lessons', 'LessonController@index');
-Route::get('/lesson/{id}', 'LessonController@show');
+Route::get('/lesson/{lesson}', 'LessonController@show');
 Route::get('/lessons/unsubmitted', 'LessonController@unsubmitted');
 Route::get('/lessons/submitted', 'LessonController@submitted');
 Route::post('/lesson', 'LessonController@store');
 Route::put('/lesson/{lesson}', 'LessonController@update');
 
+//note routes
+Route::get('/note/{note}', 'NoteController@show');
 Route::post('/note', 'NoteController@store');
+Route::put('/note/{note}', 'NoteController@update');
+Route::delete('/note/{note}', 'NoteController@destroy');
 
+//student routes
 Route::get('/students', 'StudentController@index');
-Route::get('/student/{id}', 'StudentController@show');
+Route::get('/student/{student}', 'StudentController@show');
 Route::post('/student', 'StudentController@store');
 Route::put('/student/{student}', 'StudentController@update');
