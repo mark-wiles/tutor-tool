352c78426cdcbab8d7a899a84305ea97c14b2ad9
phpdev115 <goldendiscount01@gmail.com>
2019-02-21 09:21:27 -0800

    require https in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+         // Force SSL in production
+         if ($this->app->environment() == 'production') {
+            
+            URL::forceScheme('https');
+        }
     }
 
     /**
