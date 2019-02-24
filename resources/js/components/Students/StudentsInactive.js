b5568aa1e443d31b88d56d0e5a6c49c8d2bf8359
phpdev115 <goldendiscount01@gmail.com>
2019-02-23 23:06:45 -0800

    added view for inactive students

---
+++ resources/js/components/Students/StudentsInactive.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import NavbarTop from '../NavbarTop';
+
+class StudentsInactive extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			students: []
+		};
+	}
+
+	componentDidMount() {
+		axios.get('/api/students/inactive'
+		)
+		.then((response) => {
+			var students = response.data;
+			this.setState({students});
+		})
+		.catch((error) => {
+			console.log(error)
+		});
+	}
+
+	render() {
+
+		const allStudents = this.state.students.map((student) =>
+			
+				<div className="student-summary" key={student.id}>
+					<Link to={'/student/' + student.id}>
+						<h4 className="font-weight-bold mb-1">{student.first_name} {student.last_name}</h4>
+						<h6 className="mb-0">{student.addresses[0] ? student.addresses[0].city + ', ' : ''}{student.addresses[0] ? student.addresses[0].state : ''}</h6>
+					</Link>
+				</div>
+		)
+
+		return (
+			<div className="row">
+				<NavbarTop classLeft="fas fa-cog orange" classRight="fas fa-plus orange" linkLeft="/settings" linkRight="/student/new" title="Inactive" />
+
+				<div className="container content-container">
+					{ allStudents }
+				</div>
+			</div>
+		);
+	}
+}
+
+export default StudentsInactive;
