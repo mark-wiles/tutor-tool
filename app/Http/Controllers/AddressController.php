c4743926733cc34f107cb1f0a3348b409b9a9d0a
phpdev115 <goldendiscount01@gmail.com>
2019-03-24 00:36:26 -0700

    added method to get addresses belonging to a particular student

--- app/Http/Controllers/AddressController.php
+++ app/Http/Controllers/AddressController.php
@@ -12,9 +12,11 @@ class AddressController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index($id)
     {
-        //
+        $addresses = Address::where(['student_id' => $id, 'user_id' => auth()->id()])->get();
+
+        return ($addresses);
     }
 
     /**
