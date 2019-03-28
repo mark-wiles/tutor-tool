ee34baeca5caff32640c8de475a600caf6056b4e
phpdev115 <goldendiscount01@gmail.com>
2019-03-28 13:40:13 -0700

    added padding to the NavbarTop title

--- resources/js/components/NavbarTop.js
+++ resources/js/components/NavbarTop.js
@@ -6,7 +6,7 @@ const NavbarTop = (props) => (
 
 			<nav className="container nav-top">
 				<Link to={props.linkLeft}><Icon className={props.classLeft} /></Link>
-				<h2 className="mb-0 text-center">{props.title}</h2>
+				<h2 className="mb-0 pl-3 pr-3 text-center">{props.title}</h2>
 				<Link to={props.linkRight}><Icon className={props.classRight} /></Link>
 			</nav>
 		)
